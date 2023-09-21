60e1a957bd425670c613235d556ef80f95f09cd5
charleysanchez <140567706+charleysanchez@users.noreply.github.com>
2023-09-20 23:15:43 -0700

    lots o changes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from cs50 import SQL
 from flask import Flask, redirect, render_template, request
-from helpers import get_game_data
+from helpers import get_game_data, get_champion_data, get_champion_icons, get_item_icons, get_summoner_by_puuid, get_summoner_by_username, get_summoner_history, get_match_info_by_id
+import os
+import urllib3
 
+urllib3.disable_warnings()
 app = Flask(__name__)
 
 game_data = SQL("sqlite:///game_data.db")
@@ -10,21 +13,27 @@ game_data.execute(
     "DELETE FROM champions"
 )
 
+api_key = "RGAPI-3fe0c773-c6d2-4c2f-a39e-32dd0baf1324"
+
 active_player = get_game_data()["activePlayer"]
 all_players = get_game_data()["allPlayers"]
+champion_data = get_champion_data()
 
 for player in all_players:
     game_data.execute(
-        "INSERT INTO champions (summoner_name, champion_name) VALUES (?, ?);",
+        "INSERT INTO champions (summoner_name, champion_name, level, kills, deaths, assists, creepscore) VALUES (?, ?, ?, ?, ?, ?, ?);",
         player["summonerName"],
-        player["championName"])
+        player["championName"],
+        player["level"],
+        player["scores"]["kills"],
+        player["scores"]["deaths"],
+        player["scores"]["assists"],
+        player["scores"]["creepScore"],
+        )
     
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        redirect("/champions")
-
     return render_template("index.html")
 
 @app.route("/champions", methods=["GET", "POST"])
@@ -33,8 +42,120 @@ def champions():
         "SELECT summoner_name FROM champions;"
     )
     champions = game_data.execute(
-        "SELECT champion_name FROM champions"
+        "SELECT champion_name, level, kills, deaths, assists, creepscore FROM champions"
     )
-    names = zip(summoners, champions)
 
-    return render_template("champions.html", names=names)
+    for champion in champions:
+        champion_name = champion["champion_name"].replace(' ', '')
+        if champion_name == "Cho'Gath":
+            champion_name = "Chogath"
+
+        get_champion_icons(champion_name)
+        champion["champ_img"] = champion_name
+    names = zip(summoners, champions)
+    folder = "static/champion_icons"
+    images_len = len([f for f in os.listdir(folder) if f.endswith('.png')])
+
+    return render_template("champions.html", names=names, images_len=images_len)
+
+@app.route("/summoner", methods=["GET", "POST"])
+def summoner():
+    if request.method == "POST":
+
+        # reset database
+        game_data.execute("DELETE FROM match;")
+
+        # get simple data like region, summoner_id (Username)
+        region = request.form.get("region")
+        summoner_id = request.form.get("summoner_id")
+
+        # request more data on the summoner (puuid, and more)
+        summoner_info = get_summoner_by_username(region, summoner_id, api_key)
+
+        # assign puuid variable to make things easier
+        puuid = summoner_info["puuid"]
+
+        # pull the match history by game id using API
+        match_history_by_id = get_summoner_history(puuid, region, api_key)
+
+        # create empty list for match_history with all data to go in
+        match_history = []
+
+        # add the most recent match to this list
+        match_history.append(get_match_info_by_id(region, match_history_by_id[0], api_key))
+
+        # go through each match in match_history list
+        for match_id in match_history:
+            match = match_id["metadata"]["matchId"]
+            game_mode = match_id["info"]["gameMode"]
+
+            # grab each participant
+            for participant in match_id["info"]["participants"]:
+                
+                # get their summoner name
+                summoner_name  = participant["summonerName"]
+
+                # get their champion played
+                champion_played = participant["championName"]
+
+                # get their score
+                level = participant["champLevel"]
+                kills = participant["kills"]
+                assists = participant["assists"]
+                deaths = participant["deaths"]
+                creepscore = participant["totalMinionsKilled"]
+                puuid = "poop"
+                team_id = participant["teamId"]
+
+                # get item information
+                items = {}
+                for i in range(7):
+                    items[f"item{i}"] = participant[f"item{i}"]
+                    get_item_icons(items[f"item{i}"])
+
+
+
+                # add to the database with match id as well
+                game_data.execute(
+                    "INSERT INTO match VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+                    summoner_name,
+                    champion_played,
+                    level,
+                    kills,
+                    deaths,
+                    assists,
+                    creepscore,
+                    puuid,
+                    match,
+                    team_id,
+                    game_mode,
+                    items["item0"],
+                    items["item1"],
+                    items["item2"],
+                    items["item3"],
+                    items["item4"],
+                    items["item5"],
+                    items["item6"]
+                )
+
+                
+
+                
+
+        players = game_data.execute("SELECT * FROM match;")
+
+        for player in players:
+                    champion_name = player["champion_name"].replace(' ', '')
+                    if champion_name == "Cho'Gath":
+                        champion_name = "Chogath"
+
+                    get_champion_icons(champion_name)
+                    player["champ_img"] = champion_name
+                    
+                         
+
+            
+
+        return render_template("summoner.html", summoner_id=summoner_id, players=players)
+
+    return render_template("summoner.html")
--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 import datetime
-import json
+import os
 import requests
 import time
 
@@ -22,8 +22,72 @@ def get_game_data():
             time.sleep(5)
             continue
 
-active_player = get_game_data()["activePlayer"]
-all_players = get_game_data()["allPlayers"]
+def get_champion_data():
+    url = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json"
 
+    # query API
+    response = requests.get(
+        url,
+        verify=False
+    )
+    return response.json()
+
+def get_champion_icons(champion):
+    url = f"http://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/{champion}.png"
+    
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            # save image
+            image_name = f"{champion}.png"
+            folder_name = "static/champion_icons"
+            with open(os.path.join(folder_name, image_name), "wb") as f:
+                f.write(response.content)
+        else:
+            print(f"HTTP GET request failed with status code {response.status_code}")
+    
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+def get_item_icons(item_id):
+    if item_id == 0:
+        return
+    url = f"http://ddragon.leagueoflegends.com/cdn/13.18.1/img/item/{item_id}.png"
+    response = requests.get(url)
+    image_name = f"{item_id}.png"
+    folder_name = "static/item_icons"
+    with open(os.path.join(folder_name, image_name), "wb") as f:
+        f.write(response.content)
+
+def get_match_info_by_id(region, match_id, api_key):
+    if region in ['NA']:
+        url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={api_key}"
+    response = requests.get(url)
+    return response.json()
+
+def get_summoner_by_username(region, summoner_id, api_key):
+    url = f"https://{region}1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_id}?api_key={api_key}"
+    response = requests.get(url)
+    return response.json()
+
+def get_summoner_by_puuid(region, puuid, api_key):
+    url = f"https://{region}1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={api_key}"
+    response = requests.get(url)
+    return response.json()
+
+def get_summoner_history(puuid, region, api_key):
+    if region in ['NA']:
+        url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key={api_key}"
+    response = requests.get(url)
+    return response.json()
+
+
+
+url = 'https://americas.api.riotgames.com/lol/match/v5/matches/NA1_4767732253?api_key=RGAPI-3fe0c773-c6d2-4c2f-a39e-32dd0baf1324'
+response = requests.get(url)
+response = response.json()
+print(response["metadata"]["matchId"])
 
     
